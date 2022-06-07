74206814ec6b4c9a19510faed3e23e48fce7fded
pepamatousek <jmatousek.jobs@icloud.com>
2022-06-07 22:55:21 +0200

    Cena se dvěma desetinnými čísly

--- show_btc_price.py
+++ show_btc_price.py
@@ -11,7 +11,7 @@ def main(url, sec):
         to_json = coinbase.json()
         price = to_json["data"]["amount"]
         currency = to_json["data"]["currency"]
-        print(f"Current bitcoin price from Coinbase is {price} {currency}")
+        print(f"Current bitcoin price from Coinbase is {float(price):.2f} {currency}")
         time.sleep(sec)
 
 
